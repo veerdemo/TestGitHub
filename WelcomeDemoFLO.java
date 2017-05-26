87cd41f3a11b8b78dd76f6eff441272780efe76a
veerdemo <veer.abhinav@fisglobal.com>
2017-05-26 14:03:42 +0530

    Update WelcomeDemoFLO.java

--- WelcomeDemoFLO.java
+++ WelcomeDemoFLO.java
@@ -3,7 +3,7 @@ package com.remote.github.demo;
 public class WelcomeMessageFromRemote {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome message from DemoFLO branch from local to Remote");
+		System.out.println("Merge message from DemoFLO branch from local to Remote");
 
 	}
 
