9486339dd5d8392af72c8395a8260304541952bd
e3004742 <veer.abhinav@fisglobal.com>
2017-05-26 14:15:05 +0530

    Commit from Synchronize view

--- src/com/remote/github/demo/WelcomeMessage.java
+++ src/com/remote/github/demo/WelcomeMessage.java
@@ -3,7 +3,7 @@ package com.remote.github.demo;
 public class WelcomeMessage {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the DemoFLO Branch");
+		System.out.println("Welcome to the DemoFLO Branch Merge changes from Another branch");
 		
 	}
 
