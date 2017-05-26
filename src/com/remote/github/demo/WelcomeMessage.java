46508fdbf2b1996447406bd7a77571150e40db2f
e3004742 <veer.abhinav@fisglobal.com>
2017-05-26 11:22:29 +0530

    Added a message.

--- src/com/remote/github/demo/WelcomeMessage.java
+++ src/com/remote/github/demo/WelcomeMessage.java
@@ -4,6 +4,7 @@ public class WelcomeMessage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Demo:-Cloning of Remote repository and Check in");
+		System.out.println("Added message to test revert task");
 
 	}
 
