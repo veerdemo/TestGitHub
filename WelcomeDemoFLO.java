14e1e30c37a0696adcbc181a8efa2f47293ba1e0
e3004742 <veer.abhinav@fisglobal.com>
2017-05-26 14:20:58 +0530

    Merged data from DEMOFLO branch into master.Commit changes into master branch.

---
+++ WelcomeDemoFLO.java
@@ -0,0 +1,10 @@
+package com.remote.github.demo;
+
+public class WelcomeMessageFromRemote {
+
+	public static void main(String[] args) {
+		System.out.println("Merge message from DemoFLO branch from local to Remote");
+
+	}
+
+}
